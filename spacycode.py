a98a5ede58ba11b82dbe8727de7012d9e80e61af
Lyam <4368099+LyamDolk@users.noreply.github.com>
2025-01-16 20:17:54 +0100

    its working now

--- spacycode.py
+++ spacycode.py
@@ -1,7 +1,13 @@
 import spacy
 
 spacy.prefer_gpu()
-nlp = spacy.load("en_core_web_lm")
+#nlp = spacy.load("en_core_web_lm")
 #nlp = spacy.load("en_core_web_sm")
-#nlp = spacy.load("en_core_web_trf")
+nlp = spacy.load("en_core_web_trf")
 
+import en_core_web_trf
+
+nlp = en_core_web_trf.load()
+#doc = nlp("This is a symptom of woke culture, where people are afraid to speak their minds.")
+doc = nlp("This is a symptom of woke culture, but I woke up tired.")
+print([(w.text, w.pos_) for w in doc])
--- systemprep/installdepvenv.py
+++ systemprep/installdepvenv.py
@@ -1,1 +1,14 @@
 python -m pip install --upgrade pip
+python pip install -U pip setuptools wheel
+pip install -U 'spacy[cuda12x]'
+pip install pygls 
+
+python -m spacy download en_core_web_trf # success
+python -m spacy download en_core_web_sm # success
+python -m spacy download en_core_web_lg # success
+
+#Validate spacy
+python -m spacy validate
+
+
+
